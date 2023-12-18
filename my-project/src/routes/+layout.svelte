77f7865d9fc84f76e5f6b8b8adee9b237d93abca
Ebbsteer <Ebba.elversson@gmail.com>
2023-12-18 15:06:51 +0100

    frontend

--- my-project/src/routes/+layout.svelte
+++ my-project/src/routes/+layout.svelte
@@ -1,5 +1,85 @@
 <script>
     import "../app.css";
+
+    const AddTask = () => {
+        const AddTask = document.getElementById("AddTask");
+        AddTask.classList.toggle("hidden");
+    }
+
+    const ToggleToday = () => {
+        const Today = document.getElementById("Today");
+        Today.classList.toggle("hidden");
+    }
+
+    const ToggleWeek = () => {
+        const Week = document.getElementById("Week");
+        Week.classList.toggle("hidden");
+    }
+
+    const ToggleMonday = () => {
+        const Monday = document.getElementById("Monday");
+        Monday.classList.toggle("hidden");
+    }
+
+    const ToggleTuesday = () => {
+        const Tuesday = document.getElementById("Tuesday");
+        Tuesday.classList.toggle("hidden");
+    }
+
+    const ToggleWednesday = () => {
+        const Wednesday = document.getElementById("Wednesday");
+        Wednesday.classList.toggle("hidden");
+    }
+
+    const ToggleThursday = () => {
+        const Thursday = document.getElementById("Thursday");
+        Thursday.classList.toggle("hidden");
+    }
+
+    const ToggleFriday = () => {
+        const Friday = document.getElementById("Friday");
+        Friday.classList.toggle("hidden");
+    }
+
+    const ToggleSaturday = () => {
+        const Saturday = document.getElementById("Saturday");
+        Saturday.classList.toggle("hidden");
+    }
+
+    const ToggleSunday = () => {
+        const Sunday = document.getElementById("Sunday");
+        Sunday.classList.toggle("hidden");
+    }
 </script>
-  
-<slot />
+
+<div class="w-full max-w-sm mx-auto bg-black text-sm">
+    <!--Header -->
+    <div class="w-full h-14 grid place-items-center bg-[#380C47]">
+        <h1 class="text-[#130418] text-3xl font-mono font-extrabold">Lonly</h1>
+    </div>
+
+    <!--Content-->
+    <div class="flex"> 
+
+        <!--Sidebar-->
+        <div class="basis-[60px] w-12 h-[600px] bg-[#130418] pt-1 font-mono text-white">
+            <button on:click={ToggleToday} class="w-10 h-10 mx-auto my-2 bg-[#B08545] rounded-lg grid place-items-center text-[12px]">Today</button>
+            <button on:click={ToggleWeek} class="w-10 h-10 mx-auto my-2 bg-[#B08545] rounded-lg grid place-items-center text-[12px]">Week</button>
+            <button on:click={ToggleMonday} class="w-10 h-10 mx-auto my-2 bg-teal-800 rounded-lg font-extrabold grid place-items-center">M</button>
+            <button on:click={ToggleTuesday} class="w-10 h-10 mx-auto my-2 bg-sky-800 rounded-lg font-extrabold grid place-items-center">T</button>
+            <button on:click={ToggleWednesday} class="w-10 h-10 mx-auto my-2 bg-indigo-800 rounded-lg font-extrabold grid place-items-center">W</button>
+            <button on:click={ToggleThursday} class="w-10 h-10 mx-auto my-2 bg-purple-700 rounded-lg font-extrabold grid place-items-center">T</button>
+            <button on:click={ToggleFriday} class="w-10 h-10 mx-auto my-2 bg-pink-800 rounded-lg font-extrabold grid place-items-center">F</button>
+            <button on:click={ToggleSaturday} class="w-10 h-10 mx-auto my-2 bg-red-800 rounded-lg font-extrabold grid place-items-center">S</button>
+            <button on:click={ToggleSunday} class="w-10 h-10 mx-auto my-2 bg-orange-800 rounded-lg font-extrabold grid place-items-center">S</button>
+
+            <button class="w-10 h-10 mx-auto my-2 bg-[#B08545] rounded-lg grid place-items-center text-[12px]" on:click={AddTask}>Add</button>
+        </div>
+
+        <!--Lists-->
+        <div class="basis-10/12 mx-auto">
+            <slot />
+        </div>
+        
+    </div>
+</div>
--- my-project/src/routes/+page.svelte
+++ my-project/src/routes/+page.svelte
@@ -1,5 +1,220 @@
-<h1>Welcome to your library project</h1>
-<p class="text-3xl">
-	Create your package using @sveltejs/package and preview/showcase your work with SvelteKit
-</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	
+</script>
+
+<div class="text-white font-mono font-normal">
+
+	<div id="Today" class="hidden grid gap-2 p-4">
+		<div class="bg-[#B08545] rounded-lg p-2 w-full text-center">
+			Todays Tasks
+		</div>
+
+		<div class="bg-[#B08545] rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-[#B08545] rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-[#B08545] rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Week" class="hidden grid gap-2 p-4">
+		<div class="bg-[#B08545] rounded-lg p-2 w-full text-center">
+			Weekly Tasks
+		</div>
+
+		<div class="bg-[#B08545] rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-[#B08545] rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-[#B08545] rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Monday" class="hidden grid gap-2 p-4">
+		<div class="bg-teal-800 rounded-lg p-2 w-full text-center">
+			Mondays Tasks
+		</div>
+
+		<div class="bg-teal-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-teal-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-teal-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Tuesday" class="hidden grid gap-2 p-4">
+		<div class="bg-sky-800 rounded-lg p-2 w-full text-center">
+			Tuesdays Tasks
+		</div>
+
+		<div class="bg-sky-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-sky-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-sky-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Wednesday" class="hidden grid gap-2 p-4">
+		<div class="bg-indigo-800 rounded-lg p-2 w-full text-center">
+			Wednesdays Tasks
+		</div>
+
+		<div class="bg-indigo-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-indigo-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-indigo-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Thursday" class="hidden grid gap-2 p-4">
+		<div class="bg-purple-800 rounded-lg p-2 w-full text-center">
+			Thursdays Tasks
+		</div>
+
+		<div class="bg-purple-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-purple-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-purple-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Friday" class="hidden grid gap-2 p-4">
+		<div class="bg-pink-800 rounded-lg p-2 w-full text-center">
+			Fridays Tasks
+		</div>
+
+		<div class="bg-pink-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-pink-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-pink-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Saturday" class="hidden grid gap-2 p-4">
+		<div class="bg-red-800 rounded-lg p-2 w-full text-center">
+			Saturdays Tasks
+		</div>
+
+		<div class="bg-red-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-red-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-red-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="Sunday" class="hidden grid gap-2 p-4">
+		<div class="bg-orange-800 rounded-lg p-2 w-full text-center">
+			Sundays Tasks
+		</div>
+
+		<div class="bg-orange-800 rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<div class="bg-orange-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>
+				<div class="bg-orange-800 rounded-lg p-[6px]">
+					<p class="text-white">Task Name</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="AddTask" class="hidden grid gap-2 p-4">
+		<div class="bg-[#B08545] rounded-lg p-2 w-full text-center">
+			Add Task
+		</div>
+
+		<div class="bg-[#B08545] rounded-lg p-[4px]">
+			<div class="bg-black rounded-md p-[6px] grid gap-2">
+				<form action="/" method="POST" class="grid gap-y-2 grid-cols-1">
+
+					<div class="bg-[#B08545] rounded-lg p-[6px] grid grid-cols-7">
+						<input type="text" name="task" id="task" placeholder="Task Name" class="col-span-5 bg-transparent px-1"/>
+						<input type="number" name="points" id="points" placeholder="Points" class="col-span-2 bg-transparent px-1 text-center"/>
+					</div>
+
+					<div class="bg-[#B08545] rounded-lg p-[6px] grid grid-cols-7 text-center text-[10px]">
+						<div>
+							<label for="monday">Mon</label>
+							<input type="checkbox" name="monday" id="monday">
+						</div>
+
+						<div>
+							<label for="tuesday">Tue</label>
+							<input type="checkbox" name="tuesday" id="tuesday">
+						</div>
+
+						<div>
+							<label for="wednesday">Wed</label>
+							<input type="checkbox" name="wednesday" id="wednesday">
+						</div>
+
+						<div>
+							<label for="thursday">Thu</label>
+							<input type="checkbox" name="thursday" id="thursday">
+						</div>
+
+						<div>
+							<label for="friday">Fri</label>
+							<input type="checkbox" name="friday" id="friday">
+						</div>
+
+						<div>
+							<label for="saturday">Sat</label>
+							<input type="checkbox" name="saturday" id="saturday">
+						</div>
+
+						<div>
+							<label for="sunday">Sun</label>
+							<input type="checkbox" name="sunday" id="sunday">
+						</div>
+					</div>
+
+					<div class="bg-[#B08545] rounded-lg p-[6px] grid grid-cols-7">
+						<input type="submit" name="submit" id="submit" value="Add Task" class="col-span-7 bg-transparent px-1"/>
+					</div>	
+					
+				</form>
+			</div>
+		</div>
+	</div>
+	
+</div>
