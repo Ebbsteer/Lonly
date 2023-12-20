ba7f81dd7a824aedf5c139c0f8c924476706ea7e
Ebbsteer <Ebba.elversson@gmail.com>
2023-12-20 12:34:46 +0100

    Fix

--- my-project/src/lib/store.js
+++ my-project/src/lib/store.js
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+const storedTasks = JSON.parse(localStorage.getItem("tasks")) ?? { allTasks: [] };
 export const tasks = writable(storedTasks);
 tasks.subscribe(value => {
     localStorage.setItem("tasks", JSON.stringify(value));
